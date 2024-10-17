3ea316d2f6e99686d9f4ba7a4bc76ff73e185fc8
Damien Chauvet <chauvet.damien@gmail.com>
2024-10-17 14:32:34 -0400

    change test to fail

--- tests/test_api.py
+++ tests/test_api.py
@@ -13,4 +13,4 @@ def client():
 def test_read_root(client):
     response = client.get("/")
     assert response.status_code == 200
-    # assert response.json() == {'message': 'Hello World'}
+    assert response.json() == {'message': 'Hello World !'}
