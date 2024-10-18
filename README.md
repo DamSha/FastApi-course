c70dc507cbd47225d00d5813d107cfec3f6d1de2
Damien Chauvet <chauvet.damien@gmail.com>
2024-10-18 11:15:22 -0400

    Add Rate Limit to secure API calls

---
+++ README.md
@@ -0,0 +1,10 @@
+# FastAPI Course
+
+----
+
+## Install Step for CI/CD on Heroku
+
+- Github Actions :
+[.github/workflows/main.yml](.github%2Fworkflows%2Fmain.yml)
+
+- 
--- app/security--api_key.py
+++ app/security--api_key.py
@@ -1,13 +1,27 @@
 import secrets
 
 import uvicorn
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, Response, status
 from fastapi.security import OAuth2PasswordBearer
+from fastapi.requests import Request
 from pydantic import BaseModel
-from starlette import status
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 
-# https://www.youtube.com/watch?v=0sOvCWFmrtA
 app = FastAPI()
+# Add Rate Limiter
+# Max 3/sec : 1 token_expired + 1 get_token + 1 API call
+limiter = Limiter(key_func=get_remote_address,
+                  default_limits=["3/second"],
+                  )
+# limiter = Limiter(key_func=get_remote_address)
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+# Add Limiter Globally
+# Send 429 status with details {"error":"Rate limit exceeded: 1 per 1 second"}
+app.add_middleware(SlowAPIMiddleware)
 
 # Encrypted Tokens from the database
 token = secrets.token_urlsafe(32)
@@ -31,17 +45,24 @@ class Post(BaseModel):
 
 
 @app.get("/")
-async def root():
+async def root(request: Request, response: Response):
     return {"message": "Hello World"}
 
 
+@app.get("/health_check")
+# @limiter.exempt
+async def root(request: Request, response: Response):
+    # Do Health Check...
+    return {"message": "Health Check OK !"}
+
+
 @app.get("/posts/", dependencies=[Depends(api_key_auth)])
-async def get_posts() -> dict:
+async def get_posts(request: Request, response: Response) -> dict:
     return {"message": f"all posts"}
 
 
 @app.post("/posts/")
-async def save_post(new_post: Post):
+async def save_post(request: Request, response: Response, new_post: Post):
     return {"message": f"Hello {new_post.model_dump_json()}"}
 
 
